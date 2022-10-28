a49f41cf1865a6bca0f1e011a90c7903ce4421c1
Kevin1151912 <100525817+Kevin1151912@users.noreply.github.com>
2022-10-28 11:03:33 -0500

    subir

---
+++ src/Model/Conexion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author kevin
+ */
+public class Conexion {
+
+    private static Connection c = null;
+    
+    public static Connection obtener() throws SQLException, ClassNotFoundException{
+        if( c == null ){
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                c = DriverManager.getConnection("jdbc:mysql://localhost/parqueadero","root","");
+            } catch(SQLException ex){
+                throw new SQLException(ex);
+            } catch(ClassNotFoundException ex){
+                throw new ClassCastException(ex.getMessage());
+            }
+        } 
+        return c;
+    }
+    
+    public static void cerrar() throws SQLException{
+        if( c != null ){
+            c.close();
+        }
+    }
+    
+}  
---
+++ src/Model/VehiculoDTO.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author kevin
+ */
+public class VehiculoDTO {
+    
+    private String placa;
+    private String modelo;
+    private String fecha;
+    private String tipo;
+
+    public VehiculoDTO() {
+    }
+
+    public VehiculoDTO(String placa, String modelo, String fecha, String tipo) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.fecha = fecha;
+        this.tipo = tipo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+}
